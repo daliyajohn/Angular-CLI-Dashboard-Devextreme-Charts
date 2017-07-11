e1297688701dd147026dabe27e7b9aebd5e83a27
daliyajohn <daliyajohn75@gmail.com>
2017-07-11 15:27:37 +0530

    Integrate Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MdCardModule } from '@angular/material';
+import { MdMenuModule } from '@angular/material';
+import { MdInputModule } from '@angular/material';
+import { MdButtonModule } from '@angular/material';
+import { MdListModule } from '@angular/material';
+import { MdIconModule } from '@angular/material';
+import { MdSidenavModule } from '@angular/material';
+import { MdToolbarModule } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +23,16 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdCardModule, 
+    MdMenuModule,
+    MdInputModule, 
+    MdButtonModule,
+    MdListModule,
+    MdIconModule,
+    MdSidenavModule,
+    MdToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
