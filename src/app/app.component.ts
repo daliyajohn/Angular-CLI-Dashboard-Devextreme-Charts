2238146d351c2586a39eed1df6e8d6242874cfd5
daliyajohn <daliyajohn75@gmail.com>
2017-08-28 11:59:22 +0530

    Sidenav UI development fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,10 @@ import { AppService } from './app.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	showMenu: boolean = false;
+    showTable:boolean = false;
+	public isTlbOpen:any;
+  	public isTlbClose:any;
 	constructor( public service: AppService) {
     	this.service.showMenu.emit(false);
 	}
